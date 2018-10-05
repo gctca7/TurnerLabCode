dc43ad6c109155f592b56243b178158d54e41fd3
gctca7 <35080939+gctca7@users.noreply.github.com>
2018-10-05 15:11:57 -0400

    Add files via upload

--- ArduinoHelp.m
+++ ArduinoHelp.m
@@ -1,11 +1,14 @@
-serial_port = 'COM8' ;              % I CHECKED WHICH PORT TO USE JUST BY UNPLUGGING THE DEVICE
-dev = ModularClient(serial_port) ;  % CREATES A CLIENT OBJECT
+% THIS IS BASIC CODE TO COMMUNICATE WITH THE PowerSwitchController VIA THE SERIAL PORT.  
+
+serial_port = 'COM8' ;              % I CHECKED WHICH WAS THE RIGHT COM PORT JUST BY UNPLUGGING THE DEVICE AND SEEING WHAT WENT AWAY
+dev = ModularClient(serial_port) ;  % CREATES A CLIENT OBJECT THAT ALLOWS YOU TO TALK TO THE DEVICE
 dev.open()
 dev.getDeviceId()
-dev.getMethods()                 % get device methods
+dev.getMethods()                    % RETURNS THE DEVICE METHODS
 
-% MORE DETAILED HELP
+% IF YOU WANT MORE DETAILED HELP ON THE METHOD ITSELF
 method_info = dev.startPwm('?')
+% IF YOU WANT MORE UNDERSTANDING OF THE INPUT PARAMETERS:
 parameter_info = dev.setChannelsOn('channels','?')
 
 % DETAILED HELP FOR MOTOR DRIVER:
@@ -16,5 +19,5 @@ method_info = dev.moveBy('?')
 method_info = dev.homeNozzle('?')
 
 % CLOSE EVERYTHING WHEN FINISHING
-dev.close()                      % close serial connection
-delete(dev)                      % deletes the client
+dev.close()                      % CLOSE SERIAL CONNECTION
+delete(dev)                      % DELETE THE CLIENT
