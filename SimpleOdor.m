96b3b09f76b666c623d95b8d2a416f0f94766d61
gctca7 <35080939+gctca7@users.noreply.github.com>
2018-10-05 15:11:19 -0400

    Add files via upload

---
+++ SimpleOdor.m
@@ -0,0 +1,54 @@
+function SimpleOdor(OdorOnset, OdorDuration, InterTrialInterval, NumTrials, varargin)
+%%
+% varagin: Odors to be delivered.  Variable length input so 1 or 1,2 or 1,2,3 etc
+% OdorOnset: Time when valves open in SECONDS
+% OdorDuration: In SECONDS
+% InterTrialInterval: SECONDS between Trial starts
+% NumTrials: Number of odor presentation repeats.  NOTE: Odors delivered
+% in blocks
+%%
+if (nargin < 5 | nargin > 8)
+    error('I can only deliver between 1 and 4 odors')
+end
+if InterTrialInterval < OdorDuration+OdorOnset
+    error('Your Inter Trial Interval too short')
+end
+
+NumberOdors = nargin - 4 ;
+disp(['Presenting ' num2str(NumberOdors) ' odors'])
+
+% ESTABLISH COMMUNICATION WITH ARDUINO
+% getAvailableComPorts()
+serial_port = 'COM5'                % I CHECKED WHICH PORT TO USE JUST BY UNPLUGGING THE DEVICE
+dev = ModularClient(serial_port) ;  % CREATES A CLIENT OBJECT
+dev.open()
+
+% CHANNEL CONFIGURATIONS
+% THIS DESCRIBES WHICH OPEN/CLOSED STATE OF THE FOUR VALVES FOR EACH OF THE FOUR DIFFERENT ODORS
+% Odor A: valve1/Op v2/cl v3/cl v4/cl
+% Odor B: valve1/Op v2/cl v3/Op v4/cl
+% Odor C: valve1/Op v2/Op v3/cl v4/cl
+% Odor D: valve1/Op v2/Op v3/cl v4/Op
+
+% INDICES FOR WHICH VALVES TO OPEN FOR EACH ODOR PORT:
+OdorA = {[0]} ;
+OdorB = {[0 2]} ;
+OdorC = {[0 1 3]} ;
+OdorD = {[0 1]} ;
+ValveConfigs = {OdorA OdorB OdorC OdorD} ;
+
+for OdorIdx = 1:NumberOdors
+    for n = 1:NumTrials
+        pause(OdorOnset) ;
+        dev.setChannelsOn(ValveConfigs{OdorIdx}) ;  %(ValveConfigs{OdorIdx}) MAY NOT BE THE RIGHT SYNTAX
+        pause(OdorDuration)
+        dev.setAllChannelsOff() ;
+        pause(InterTrialInterval - OdorDuration)
+        disp(['Trial 'num2str(OdorIdx*NumTrials) 'of ' num2str(NumberOdors*NumTrials)]) ; 
+    end
+end
+
+% CLOSE COMMUNICATION WITH ARDUINO
+dev.close()                      % CLOSE SERIAL CONNECTION
+delete(dev)                      % DELETE THE CLIENT
+
